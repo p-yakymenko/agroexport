1f2ed8daa8725f07967c7f148903f15f3465e02d
Vyacheslav <vyacheslavfil@ukr.net>
2018-06-19 18:44:48 +0300

    migrations

---
+++ database/migrations/2018_06_19_122114_create_sellers_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSellersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sellers', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('address');
+            $table->string('country');
+            $table->string('phone');
+            $table->string('email');
+            $table->string('site')->nullable();
+            $table->string('activity_type')->nullable();
+            $table->string('contact_person')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sellers');
+    }
+}
---
+++ database/migrations/2018_06_19_122421_create_sellers_to_product_categories_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSellersToProductCategoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sellers_to_product_categories', function (Blueprint $table) {
+            $table->integer('seller_id')->unsigned();
+            $table->integer('product_category_id')->unsigned();
+            $table->foreign('seller_id')->references('id')->on('sellers');
+            $table->foreign('product_category_id')->references('id')->on('product_categories');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('sellers_to_product_categories', function (Blueprint $table) {
+          $table->dropForeign('sellers_to_product_categories_product_category_id_foreign');
+      });
+        Schema::table('sellers_to_product_categories', function (Blueprint $table) {
+          $table->dropForeign('sellers_to_product_categories_seller_id_foreign');
+      });
+        Schema::dropIfExists('sellers_to_product_categories');
+    }
+}
