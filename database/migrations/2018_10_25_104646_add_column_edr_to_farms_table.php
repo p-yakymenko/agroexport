9bd8e94cf156921bebe603ea363d46b2d38d6bf7
p-yakymenko <yackimenko.pavel@gmail.com>
2018-10-25 15:52:13 +0300

    Fix migration

--- database/migrations/2018_10_25_104646_add_column_edr_to_farms_table.php
+++ database/migrations/2018_10_25_104646_add_column_edr_to_farms_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddColumnToFarmsTable extends Migration
+class AddColumnEdrToFarmsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -25,6 +25,8 @@ class AddColumnToFarmsTable extends Migration
      */
     public function down()
     {
-        //
+        Schema::table('farms', function (Blueprint $table) {
+            $table->dropColumn('edrpou');
+        });
     }
 }
